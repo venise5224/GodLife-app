b965a033941c1c570c2c12379ad4a27bfc7244ee
JSM <JSM@>
2025-09-20 18:00:59 +0900

    feat: 활동 보드와 랜더 틀 구현

---
+++ components/ActivityBoard/ActivityBoard.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
+
+export default function ActivityBoard() {
+  const [boardTab, setBoardTab] = useState<"logger" | "planner">("logger");
+
+  return (
+    <View className="w-full">
+      {/* 탭 버튼 */}
+      <View className="flex flex-row">
+        <Pressable
+          className={`flex-1 p-2 items-center ${
+            boardTab === "logger"
+              ? "bg-white border-2 border-b-0"
+              : "bg-gray-200 border-b-2"
+          }`}
+          onPress={() => setBoardTab("logger")}
+        >
+          <Text
+            className={`${
+              boardTab === "logger" ? "text-black" : "text-gray-700"
+            }`}
+          >
+            할 일 기록
+          </Text>
+        </Pressable>
+
+        <Pressable
+          className={`flex-1 p-2 items-center ${
+            boardTab === "planner"
+              ? "bg-white border-2 border-b-0"
+              : "bg-gray-200 border-b-2"
+          }`}
+          onPress={() => setBoardTab("planner")}
+        >
+          <Text
+            className={`${
+              boardTab === "planner" ? "text-black" : "text-gray-700"
+            }`}
+          >
+            할 일 계획
+          </Text>
+        </Pressable>
+      </View>
+
+      {/* 탭 컨텐츠 */}
+      <View className="border-2 border-t-0 rounded-b-2xl p-4">
+        {boardTab === "logger" ? (
+          <View>활동 기록</View>
+        ) : (
+          <View>활동 계획</View>
+        )}
+      </View>
+    </View>
+  );
+}
